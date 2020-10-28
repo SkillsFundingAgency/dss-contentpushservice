1ce2f85c6c710297ed3671dcafe57145587bc7ec
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2020-10-28 11:46:15 +0000

    Handle httpclientfactory.send exception.

--- NCS.DSS.ContentPushService/Utils/DigitalIdentityClient.cs
+++ NCS.DSS.ContentPushService/Utils/DigitalIdentityClient.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,54 +21,73 @@ namespace NCS.DSS.ContentPushService.Utils
         public async Task<bool> Post(string customerId, string objectId, string body, string endpoint)
         {
             var response = await Send(HttpMethod.Post, endpoint, body);
-            var respstr = await response.Content.ReadAsStringAsync();
-            var isSuccess = response.StatusCode == System.Net.HttpStatusCode.OK;
+            var respstr = await GetResponseContent(response);
+            var isSuccess = IsSuccessfulResponse(response, HttpStatusCode.OK);
             if (!isSuccess)
-                _logger.LogInformation($"Failed to post customerId: {customerId}, ObjectId: {objectId} - Status: {response.StatusCode} response is: {respstr} ");
+                _logger.LogInformation($"Failed to post customerId: {customerId}, ObjectId: {objectId} - Status: {response?.StatusCode} response is: {respstr} ");
             return isSuccess;
         }
 
         public async Task<bool> Patch(string customerId, string objectId, string body, string endpoint)
         {
             var response = await Send(HttpMethod.Patch, endpoint, body);
-            var respstr = await response.Content.ReadAsStringAsync();
-            var isSuccess = response.StatusCode == System.Net.HttpStatusCode.OK;
+            var respstr = await GetResponseContent(response);
+            var isSuccess = IsSuccessfulResponse(response, HttpStatusCode.OK);
             if (!isSuccess)
-                _logger.LogInformation($"Failed to Patch json {body} - Status: {response.StatusCode}  response is: {respstr} ");
+                _logger.LogInformation($"Failed to Patch json {body} - Status: {response?.StatusCode}  response is: {respstr} ");
             return isSuccess;
         }
 
         public async Task<bool> Delete(string customerId, string objectId, string body, string endpoint)
         {
             var response = await Send(HttpMethod.Delete, endpoint, body);
-            var respstr = await response.Content.ReadAsStringAsync();
-            var isSuccess = response.StatusCode == System.Net.HttpStatusCode.OK;
+            var respstr = await GetResponseContent(response);
+            var isSuccess = IsSuccessfulResponse(response, HttpStatusCode.OK);
             if (!isSuccess)
-                _logger.LogInformation($"Failed to delete customerId: {customerId}, objectId: {objectId} - Status: {response.StatusCode}  response is : {respstr} ");
+                _logger.LogInformation($"Failed to delete customerId: {customerId}, objectId: {objectId} - Status: {response?.StatusCode}  response is : {respstr} ");
             return isSuccess;
         }
 
         public async Task<bool> Put(string customerId, string objectId, string body, string endpoint)
         {
             var response = await Send(HttpMethod.Put, endpoint, body);
-            var respstr = await response.Content.ReadAsStringAsync();
-            var isSuccess = response.StatusCode == System.Net.HttpStatusCode.OK;
+            var respstr = await GetResponseContent(response);
+            var isSuccess = IsSuccessfulResponse(response, HttpStatusCode.OK);
             if (!isSuccess)
-                _logger.LogInformation($"Failed to put customerId: {customerId}, objectId: {objectId} - Status: {response.StatusCode}  response is : {respstr} ");
+                _logger.LogInformation($"Failed to put customerId: {customerId}, objectId: {objectId} - Status: {response?.StatusCode}  response is : {respstr} ");
             return isSuccess;
         }
 
-        private async  Task<HttpResponseMessage> Send(HttpMethod verb, string endpoint, string body)
+        private async Task<HttpResponseMessage> Send(HttpMethod verb, string endpoint, string body)
         {
-            using (var client = _httpclientFactory.CreateClient("AzureB2C"))
-            using (var request = new HttpRequestMessage(verb, endpoint))
+            try
             {
-                using (var stringContent = new StringContent(body, Encoding.UTF8, "application/json"))
+                using (var client = _httpclientFactory.CreateClient("AzureB2C"))
+                using (var request = new HttpRequestMessage(verb, endpoint))
                 {
-                    request.Content = stringContent;
-                    return await client.SendAsync(request);
+                    using (var stringContent = new StringContent(body, Encoding.UTF8, "application/json"))
+                    {
+                        request.Content = stringContent;
+                        return await client.SendAsync(request);
+                    }
                 }
+            } catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
             }
+            return null;
+        }
+
+        private bool IsSuccessfulResponse(HttpResponseMessage resp, HttpStatusCode expectedResult)
+        {
+            return resp?.StatusCode == expectedResult;
+        }
+
+        private async Task<string> GetResponseContent(HttpResponseMessage resp)
+        {
+            if(resp != null)
+                return await resp.Content.ReadAsStringAsync();
+            return string.Empty;
         }
     }
 }
